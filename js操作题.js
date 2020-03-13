c1fa9235bca6aea180b83e4623c8f107ce402660
ZhangJiL <45391763+ZhangJiL@users.noreply.github.com>
2020-03-13 16:50:44 +0800

    Update js操作题.js

--- js操作题.js
+++ js操作题.js
@@ -1,1 +1,30 @@
+// 1 深度优先遍历和广度优先遍历（以dom树为例）
+function dfs(node, nodeList = []) {
+  if(node !== null) {
+    nodeList.push(node)
+    const childs = node.children
+    for(let i=0;i<childs.length;i++) {
+      dfs(childs[i],nodeList)
+    }
+  }
+  return nodeList
+}
+// 借助队列 queue 实现(先进先出)
+function bfs(node) {
+  let nodeList = []
+  let queue = []
+  if(node) {
+    queue.push(node)
+    while(queue.length) {
+      const item = queue.shift()
+      const childs = item.children
+      nodeList.push(item)
+      for(let i=0;i<childs.length;i++) {
+        queue.push(childs[i])
+      }
+    }
+  }
+  return nodeList
+}
 
+// 2.请分别用深度优先思想和广度优先思想实现一个拷贝函数
